e8556891a8115727b82a6591c30b833f42cbd868
ddimatee <brayandimate2023@gmail.com>
2025-07-31 09:39:48 -0500

    Creo interfaz de función EliminarProducto y ajusto archivo db.js para que funcione correctamente.

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'admin',
+  password: '',
   database: 'tienda',
   charset: 'utf8mb4'
 });
